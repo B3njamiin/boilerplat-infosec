bc1bcb588594e6d7ed61d1e203708ddce86d7c70
B3njamiin <benjaminreyes.y@gmail.com>
2024-04-02 16:25:32 -0300

    test 3

--- myApp.js
+++ myApp.js
@@ -3,8 +3,7 @@ const app = express();
 const helmet = require('helmet');
 
 app.use(helmet.hidePoweredBy());
-//app.use(helmet.frameguard({ action: 'deny'}));
-app.use(helmet.xFrameOptions());
+app.use(helmet.frameguard(false));
 
 
 
